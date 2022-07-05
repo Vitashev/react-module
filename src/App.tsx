ee93b3fd4573bdb9b681dba0f24b67f9a571afeb
Vitalii Shevchuk <vitaliishevchuk@Vitaliis-MBP.attlocal.net>
2022-07-04 22:03:43 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import styles from './App.module.css'
 
 function App() {
+  const option1 = 1
+  const option2 = 2
+  const option3 = 3
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles['option-' + option1]}>Red</div>
+      <div className={styles['option-' + option2]}>Green</div>
+      <div className={styles['option-' + option3]}>Blue</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
